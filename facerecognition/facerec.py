a8bb124cfae108a82793c651b4b694e1f4fec630
eren <efrieln@gmail.com>
2023-01-18 10:20:15 +0300

    cascades

--- facerecognition/facerec.py
+++ facerecognition/facerec.py
@@ -4,8 +4,12 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 while(True):
+    #yakalama
     ret, frame = cap.read()
 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #gösterme
     cv2.imshow("frame", frame)
     if cv2.waitKey(20) & 0xFF == ord("q"):
         break
