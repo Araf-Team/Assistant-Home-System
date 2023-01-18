292d1791d0ebcb36f0a286d1a589c9e81484d834
eren <efrieln@gmail.com>
2023-01-18 11:02:52 +0300

    facerecog

--- facerecognition/facerec.py
+++ facerecognition/facerec.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 
+face_cascade = cv2.CascadeClassifier("facerecognition/cascades/data/haarcascade_frontalface_alt2.xml")
+
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -8,6 +10,19 @@ while(True):
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+h]
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+
+        color = (196, 31, 255)
+        stroke = 2
+        width = x + w
+        height = y + h
+        cv2.rectangle(frame, (x,y), (width, height), color, stroke)
 
     #gösterme
     cv2.imshow("frame", frame)
